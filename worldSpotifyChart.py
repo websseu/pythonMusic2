1210959f817c1aedf59e8a1669346ab333417a92
웹스토리보이 <webstoryboy@naver.com>
2024-12-23 20:40:20 -0800

    2024.12.23

--- worldSpotifyChart.py
+++ worldSpotifyChart.py
@@ -16,9 +16,9 @@ SPOTIFY_PASSWORD = "Forever8888!s"
 
 # 이틀 전 날짜 계산
 # current_date = datetime.now().strftime("%Y-%m-%d")
-current_date = "2024-12-22"
+current_date = "2024-12-23"
 # two_days_ago = "latest"
-two_days_ago = "2024-12-20"
+two_days_ago = "2024-12-21"
 
 # 국가별 URL 설정
 countries = {
